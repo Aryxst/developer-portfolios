ce00474c0f648af3a45095577442db74661c58ed
Jessie Odeh <101392520+Aryxst@users.noreply.github.com>
2024-04-28 13:53:39 +0200

    - add crossfade animation - scroll to top with view transitions on `page/[page]` routes - fix prettier config throwing error formating mjs

--- website/tailwind.config.mjs
+++ website/tailwind.config.mjs
@@ -3,13 +3,23 @@ import plugin from 'tailwindcss/plugin';
 export default {
  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
  theme: {
-  extend: {},
+  extend: {
+   animation: {
+    blurIn: 'blurIn 1s linear',
+   },
+   keyframes: {
+    blurIn: {
+     '0%': { filter: 'blur(3px)' },
+     '100%': { filter: 'blur(0px)' },
+    },
+   },
+  },
+  plugins: [
+   plugin(function ({ addBase }) {
+    addBase({
+     h1: { fontSize: '32px', lineHeight: '44.8px' },
+    });
+   }),
+  ],
  },
- plugins: [
-  plugin(function ({ addBase }) {
-   addBase({
-    h1: { fontSize: '32px', lineHeight: '44.8px' },
-   });
-  }),
- ],
 };
